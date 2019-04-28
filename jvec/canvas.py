962ffc951e40ea3a6809367b5eaf7f90e9974622
Jonathan Reichelt Gjertsen <jonath.re@gmail.com>
2019-04-28 20:40:28 +0200

    Add configuration and non-canvas options

--- jvec/canvas.py
+++ jvec/canvas.py
@@ -18,7 +18,6 @@ MODIFIERS = {
     16777223: 'delete'
 }
 
-
 class Qt5Painter(PainterInterface):
     def __init__(self, qwidget):
         self.qwidget = qwidget
@@ -32,8 +31,8 @@ class Qt5Painter(PainterInterface):
     def drawRect(self, x0, y0, width, height):
         self.qpainter.drawRect(x0, y0, width, height)
 
-    def drawLine(self, *args):
-        self.qpainter.drawLine(*args)
+    def drawLine(self, x0, y0, x1, y1):
+        self.qpainter.drawLine(x0, y0, x1, y1)
 
     def __enter__(self):
         self.qpainter = QPainter()
@@ -66,15 +65,19 @@ class Canvas(QWidget, Representable):
             'y0': 300,
             'grid_x': 1,
             'grid_y': 1,
+            'translation': (100, 100),
             'window_width': 450,
             'window_height': 400,
             'title': 'Editor',
+            'line_width': 10,
             **super().defaults()
         }
 
-    def __init__(self, **params):
-        QWidget.__init__(self)
+    def __init__(self, app, **params):
+        QWidget.__init__(self, app.ui.centralwidget)
         Representable.__init__(self, **params)
+
+        self.app = app
         self.shapes = {}
         self.shapes_to_create = []
         self.shapes_to_delete = []
@@ -93,7 +96,11 @@ class Canvas(QWidget, Representable):
         self.held_keys = set()
         self.painting = False
         self.grid = Grid(self)
+        self.show_grid = True
 
+    def set_grid_size(self, x, y):
+        self.grid_x = x
+        self.grid_y = y
 
     def export(self):
         data = { name: shape.export() for name, shape in self.shapes.items() }
@@ -163,10 +170,12 @@ class Canvas(QWidget, Representable):
         with Qt5Painter(self) as painter:
             self.painting = True
 
-            painter.setPen(0, 0, 0, 30)
-            self.grid.draw(painter)
+            if self.show_grid:
+                painter.setPen(0, 0, 0, 30)
+                self.grid.draw(painter)
 
             painter.setPen(0, 0, 0)
             for shape in self.shapes.values():
                 shape.draw(painter, mouse_pos=self.mouse_pos, flags=self.flags)
         self.after_painting()
+        self.app.paint_complete()
